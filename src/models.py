5091b89d993e50ff71126160b52e7668009f6db9
Javier Gracia <107503117+Javiergracia47@users.noreply.github.com>
2024-12-18 23:01:52 +0000

    proyecto finalizado

--- src/models.py
+++ src/models.py
@@ -1,32 +1,62 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class User(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+    
     id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    fecha_suscripcion = Column(DateTime(timezone=True), nullable=False)
 
-class Address(Base):
+class Planet(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name =  Column(String(250), nullable=False)
+
+
+class Character(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(250), nullable=False)
+
+class Vehicle(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(250), nullable=False)
+
+class Favorite (Base):
+    __tablename__ = 'favorite'
+
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(250), nullable=False)
+
+class Create(Base):
+    __tablename__ = 'create'
+    id = Column(Integer, primary_key=True)
+    user_id =  Column(String(250), nullable=False)
+    name =  Column(String(250), nullable=False)
+
+class Data(Base):
+    __tablename__ = 'create'
+    id = Column(Integer, primary_key=True)
+    user_id =  Column(String(250), nullable=False)
+    name =  Column(String(250), nullable=False)
+
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+## Draw from SQLAlchemy baseñ{}
 render_er(Base, 'diagram.png')
